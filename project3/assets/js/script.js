49a1f19bd0880eae1dfb9e48c91e5956f0ec6afa
lucypeterson <luciana.peterson@cca.edu>
2017-04-27 16:19:09 -0700

    donzo

--- project3/assets/js/script.js
+++ project3/assets/js/script.js
@@ -1,20 +1,15 @@
 $(document).ready(function() {
 
-	
 
-	$('.image-popup-no-margins').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		closeBtnInside: false,
-		fixedContentPos: true,
-		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-		image: {
-			verticalFit: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300 // don't foget to change the duration also in CSS
-		}
+
+// fixed
+ 	$(".bio1").click(function() {
+		$('html, body').animate({
+		scrollLeft: $(".box1").offset().left
+		}, 1000);
 	});
+
+
+// no
 
 });
